cf0fc43a3480b615dc1ebbc2bc2f2064fcae52c3
LazyDope <mydj555@gmail.com>
2023-12-01 15:27:52 -0500

    6.7

--- src/fifth.rs
+++ src/fifth.rs
@@ -1,11 +1,11 @@
-use std::ptr;
+use std::{mem, ptr};
 
 pub struct List<T> {
     head: Link<T>,
     tail: *mut Node<T>,
 }
 
-type Link<T> = Option<Box<Node<T>>>;
+type Link<T> = *mut Node<T>;
 
 struct Node<T> {
     elem: T,
@@ -15,36 +15,42 @@ struct Node<T> {
 impl<T> List<T> {
     pub fn new() -> Self {
         List {
-            head: None,
+            head: ptr::null_mut(),
             tail: ptr::null_mut(),
         }
     }
 
     pub fn push(&mut self, elem: T) {
-        let new_tail = Box::new(Node { elem, next: None });
-
-        let raw_tail: *mut _ = &mut *new_tail;
+        let raw_tail: *mut _ = Box::into_raw(Box::new(Node {
+            elem,
+            next: ptr::null_mut(),
+        }));
 
         if !self.tail.is_null() {
             unsafe {
-                (*self.tail).next = Some(new_tail);
+                (*self.tail).next = raw_tail;
             }
         } else {
-            self.head = Some(new_tail);
+            self.head = raw_tail;
         }
+
+        self.tail = raw_tail;
     }
 
-    pub fn pop(&'a mut self) -> Option<T> {
-        self.head.take().map(|head| {
-            let head = *head;
+    pub fn pop(&mut self) -> Option<T> {
+        if !self.head.is_null() {
+            let head = mem::replace(&mut self.head, ptr::null_mut());
+            let head = unsafe { *head };
             self.head = head.next;
 
-            if self.head.is_none() {
-                self.tail = None;
+            if self.head.is_null() {
+                self.tail = ptr::null_mut();
             }
 
-            head.elem
-        })
+            Some(head.elem)
+        } else {
+            None
+        }
     }
 }
 
@@ -59,15 +65,22 @@ mod tests {
 
         ll.push(1);
         ll.push(2);
-        ll.push(4);
-
-        assert_eq!(ll.pop(), Some(4));
-
         ll.push(3);
 
-        assert_eq!(ll.pop(), Some(3));
-        assert_eq!(ll.pop(), Some(2));
         assert_eq!(ll.pop(), Some(1));
+
+        ll.push(4);
+
+        assert_eq!(ll.pop(), Some(2));
+        assert_eq!(ll.pop(), Some(3));
+        assert_eq!(ll.pop(), Some(4));
+        assert_eq!(ll.pop(), None);
+
+        ll.push(5);
+        ll.push(6);
+
+        assert_eq!(ll.pop(), Some(5));
+        assert_eq!(ll.pop(), Some(6));
         assert_eq!(ll.pop(), None);
     }
 }
